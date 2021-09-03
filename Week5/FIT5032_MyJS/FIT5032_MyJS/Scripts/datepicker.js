9d514d7a49df98b90f8f572a0c0d4e55359aaa5c
SiddhivinayakKudale <skud0002@student.monash.edu>
2021-09-03 17:03:12 +0530

    updated changes

--- Week5/FIT5032_MyJS/FIT5032_MyJS/Scripts/datepicker.js
+++ Week5/FIT5032_MyJS/FIT5032_MyJS/Scripts/datepicker.js
@@ -6,3 +6,11 @@
 $("#hellobtn").click(function () {
     $("#datepicker").datepicker();
 });
+
+
+$(document).ready(function () {
+        $('#example1').datepicker({
+            autoclose: true,
+            format: "dd/mm/yyyy"
+        });
+});
